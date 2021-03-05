ed02a49839cb6345a176ec36778ceee6e5d3cddf
gustavosako <40046242+gustavosako@users.noreply.github.com>
2021-03-05 19:24:10 -0300

    Identificação e implementação de outras classes necessarias

---
+++ correcao-de-solos/src/main/java/com/gustavo/cabecalho.java
@@ -0,0 +1,16 @@
+package com.gustavo;
+
+public class cabecalho {
+    //private String produtor;
+    //private int data;
+    //private String municipio;
+    //private int lote;
+    private double areaTotal;
+    private int talhao;
+    //private int matriculaLote;
+    private int texturaSolo;
+    private int sistemadeCultivo;
+    //private String responsavelTecnico;
+    private double profundidadeAmostra;
+
+}
---
+++ correcao-de-solos/src/main/java/com/resultadoAnalise.java
@@ -0,0 +1,69 @@
+package com;
+
+public class resultadoAnalise {
+    private double quantidadeFosforo;
+    private double quantidadePotassio;
+    private double quantidadeCalcio;
+    private double quantidadeMagnesio;
+    private double quantidadeEnxofre;
+    private double quantidadeAluminio;
+    private double quantidadeAcidezPotencial;
+
+    public double getQuantidadeFosforo() {
+        return quantidadeFosforo;
+    }
+
+    public void setQuantidadeFosforo(double quantidadeFosforo) {
+        this.quantidadeFosforo = quantidadeFosforo;
+    }
+
+    public double getQuantidadePotassio() {
+        return quantidadePotassio;
+    }
+
+    public void setQuantidadePotassio(double quantidadePotassio) {
+        this.quantidadePotassio = quantidadePotassio;
+    }
+
+    public double getQuantidadeCalcio() {
+        return quantidadeCalcio;
+    }
+
+    public void setQuantidadeCalcio(double quantidadeCalcio) {
+        this.quantidadeCalcio = quantidadeCalcio;
+    }
+
+    public double getQuantidadeMagnesio() {
+        return quantidadeMagnesio;
+    }
+
+    public void setQuantidadeMagnesio(double quantidadeMagnesio) {
+        this.quantidadeMagnesio = quantidadeMagnesio;
+    }
+
+    public double getQuantidadeEnxofre() {
+        return quantidadeEnxofre;
+    }
+
+    public void setQuantidadeEnxofre(double quantidadeEnxofre) {
+        this.quantidadeEnxofre = quantidadeEnxofre;
+    }
+
+    public double getQuantidadeAluminio() {
+        return quantidadeAluminio;
+    }
+
+    public void setQuantidadeAluminio(double quantidadeAluminio) {
+        this.quantidadeAluminio = quantidadeAluminio;
+    }
+
+    public double getQuantidadeAcidezPotencial() {
+        return quantidadeAcidezPotencial;
+    }
+
+    public void setQuantidadeAcidezPotencial(double quantidadeAcidezPotencial) {
+        this.quantidadeAcidezPotencial = quantidadeAcidezPotencial;
+    }
+
+    
+}
