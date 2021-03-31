2b42d38b7b49cc5e32f29710b8ca66cdb1af147c
gustavosako <40046242+gustavosako@users.noreply.github.com>
2021-03-31 18:16:52 -0300

    classe amostraDoSolo adicionada

---
+++ correcao-de-solos/src/main/java/com/gustavo/amostraDoSolo.java
@@ -0,0 +1,48 @@
+public class amostraDoSolo {
+    private double teorFosforo;
+    private double teorPotassio;
+    private double teorCalcio;
+    private double teorMagnesio;
+    private double teorEnxofre;
+    private double teorAluminio;
+    private double teorHAL;
+
+    public amostraDoSolo (double teorFosforo, double teorPotassio, double teorCalcio, double teorMagnesio, double teorEnxofre, double teorAluminio, double teorHAL) {
+        this.teorFosforo = teorFosforo;
+        this.teorPotassio = teorPotassio;
+        this.teorCalcio = teorCalcio;
+        this.teorMagnesio = teorMagnesio;
+        this.teorEnxofre = teorEnxofre;
+        this.teorAluminio = teorAluminio;
+        this.teorHAL = teorHAL;
+    }
+
+    public double getTeorFosforo() {
+        return teorFosforo;
+    }
+
+    public double getTeorPotassio() {
+        return teorPotassio;
+    }
+
+    public double getTeorCalcio() {
+        return teorCalcio;
+    }
+
+    public double getTeorMagnesio() {
+        return teorMagnesio;
+    }
+
+    public double getTeorEnxofre() {
+        return teorEnxofre;
+    }
+
+    public double getTeorAluminio() {
+        return teorAluminio;
+    }
+
+    public double getTeorHAL() {
+        return teorHAL;
+    } 
+
+}
--- correcao-de-solos/src/main/java/com/gustavo/resultadoAnalise.java
+++ correcao-de-solos/src/main/java/com/gustavo/resultadoAnalise.java
@@ -1,4 +1,4 @@
-package com;
+package com.gustavo;
 
 public class resultadoAnalise {
     private double quantidadeFosforo;
